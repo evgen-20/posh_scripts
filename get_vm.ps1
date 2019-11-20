2a7c3b4010a8771f59e86709a7a92380e2afb810
Evgeniy <47032403+evgen-20@users.noreply.github.com>
2019-11-20 12:15:12 +0300

    Add new script

---
+++ get_vm.ps1
@@ -0,0 +1,8 @@
+﻿$a = Get-Vm | Get-Harddisk | Where-Object {$_.Filename -like "*SSD_RAID10*"} | Select Filename, CapacityGB
+$size = 0
+$a | % {
+$_.Filename
+$_.CapacityGB
+$size += $_.CapacityGB
+}
+$size
